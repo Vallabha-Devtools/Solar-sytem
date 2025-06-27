a6f508c5a163a98656fe358fec564f833f75fcd7
Vallabha-Devtools <vallabhan@devtools.in>
2025-06-27 14:32:42 +0530

    added secrets and variables for the solarsytems 2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
+mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData'
     user: ${{vars.MONGO_USERNAME}}
     password: ${{secrets.MONGO_PASSWORD}}
     useNewUrlParser: true,
