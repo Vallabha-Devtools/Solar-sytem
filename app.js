34d7643db6f89624aaf1d83f01dab7b7af4cb0b0
Vallabha-Devtools <vallabhan@devtools.in>
2025-06-27 14:47:45 +0530

    added the secrets and variables in the file

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
-    user: ${{vars.MONGO_USERNAME}}
-    password: ${{secrets.MONGO_PASSWORD}}
+mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
